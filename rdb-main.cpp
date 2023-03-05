c0a484cea3c375a160b134981184b2d3a1f18a9a
ENAMINE1 <kshashwat.iit@gmail.com>
2023-03-05 13:40:52 +0530

    updated

--- rdb-main.cpp
+++ rdb-main.cpp
@@ -63,27 +63,37 @@ int main()
     // Relation *r2 = Create_Relation();
     Relation *r1 = new Relation(2, {"name", "age"}, {1, 0}, {1, 2});
     r1->AddRecord();
+    r1->AddRecord();
+    r1->AddRecord();
+
     Relation *r2 = new Relation(2, {"name", "age"}, {0, 1}, {1, 2});
     r2->AddRecord();
+    r2->AddRecord();
+    r2->AddRecord();
+
     Basics b;
     // r1.relation_print();
     // Relation r2;
     r1->relation_print();
     r2->relation_print();
 
-   Relation* r3 = b._union(r1, r2);
-    r1->relation_print();
-    r2->relation_print();
+//    Relation* r3 = b._union(r1, r2);
+//     r1->relation_print();
+//     r2->relation_print();
 
-    if (r3 != NULL)
-    {
-        r3->relation_print();
-    }
-    else
-    {
-        cout << "UNION is not possible!"<< endl;
-    }
+//     if (r3 != NULL)
+//     {
+//         r3->relation_print();
+//     }
+//     else
+//     {
+//         cout << "UNION is not possible!"<< endl;
+//     }
     // b._rename(r1, "age", "Roll");
     // r1->relation_print();
+    Relation * r4 = b._union(r1, r2);
+    Relation *r3 = b._difference(r1, r2);
+    r4->relation_print();
+    r3->relation_print();
     return 0;
 }
--- slfs.cpp
+++ slfs.cpp
@@ -32,17 +32,18 @@ public:
     // Relation * union(Relation * R1, Relation * R2)
     Relation *_union(Relation *R1, Relation *R2)
     {
-        if (!_is_valid(R1,R2))
+        if (!_is_valid(R1, R2))
         {
             return NULL;
         }
         Relation *R3 = new Relation(*R1);
-        R3->relation_print();
+        // R3->relation_print();
         cout << endl;
         R3->nrecs += R2->nrecs;
         // adding the records of Relation R2 with the attributes in same order as in R1
         for (auto it = R2->recs.begin(); it != R2->recs.end(); it++)
         {
+            // creating a new record with entries same as in the Relation 2
             auto temp = new Record(**it);
             // temp->print_record();
             vector<Attr *> temp_vec;
@@ -54,6 +55,7 @@ public:
             for (int i = 0; i < R1->natttr; i++)
             {
                 int j = 0;
+                // here j corresponds to the index of the attribute name at index i (in Relaion 2) in Realtion 1
                 if (R2->attrnames[i] != R1->attrnames[j])
                 {
                     j++;
@@ -61,14 +63,26 @@ public:
                 temp->attrptr[R1->attrinds[j]] = temp_vec[i];
             }
             // Removing common entries
-            int flag = 0;
+            int flag = 1;
             for (auto it = R1->recs.begin(); it != R1->recs.end(); it++)
             {
+                int count = 0;
                 for (int i = 0; i < R1->natttr; i++)
                 {
-                    if (*((*it)->attrptr[i]) != *(temp->attrptr[i]))
+                    // ((*it)->attrptr[i])->print();
+                    // cout << "";
+                    // temp->attrptr[i]->print();
+                    // cout << "\t";
+                    // counting the number of common recored entries
+                    if (*((*it)->attrptr[i]) == *(temp->attrptr[i]))
                     {
-                        flag = 1;
+                        count++;
+                    }
+                    // cout << count;
+                    if (count == R1->natttr)
+                    {
+                        flag = 0;
+                        break;
                     }
                 }
             }
@@ -83,24 +97,66 @@ public:
     // Relation * difference (Relation * R1, Relation * R2)
     Relation *_difference(Relation *R1, Relation *R2)
     {
-        if (_is_valid(R1, R2))
+        Relation *R = new Relation(R1->natttr, R1->attrnames, R1->attrinds, R1->attribute_type);
+        int flag = 0;
+        if (!_is_valid(R1, R2))
         {
             return NULL;
         }
         // for each record in R1 check if it is available in R2; if not then create a new copy record of R1 and append it in the new Relation that is being returned
         // auto temp = new Record(R2);
 
-        for (auto it = R1->recs.begin(),itt = R2->recs.begin(); it != R1->recs.end(); it++, itt++)
+        for (auto it = R1->recs.begin(); it != R1->recs.end(); it++)
         {
-            int flag = 0;
+            // creating a new record with entries same as in the Relation 2
+            auto temp = new Record(**it);
+            // temp->print_record();
+            vector<Attr *> temp_vec;
             for (int i = 0; i < R1->natttr; i++)
             {
-                if (*((*it)->attrptr[i]) != *((*itt)->attrptr[i]))
+                temp_vec.push_back(temp->attrptr[i]);
+            }
+            // changing the order of elements in the record according to the elements in R1
+            for (int i = 0; i < R1->natttr; i++)
+            {
+                int j = 0;
+                // here j corresponds to the index of the attribute name at index i (in Relaion 2) in Realtion 1
+                if (R1->attrnames[i] != R2->attrnames[j])
                 {
-                    flag = 1;
+                    j++;
                 }
+                temp->attrptr[R2->attrinds[j]] = temp_vec[i];
+            }
+            // Removing common entries
+            int flag = 1;
+            for (auto it = R2->recs.begin(); it != R2->recs.end(); it++)
+            {
+                int count = 0;
+                for (int i = 0; i < R1->natttr; i++)
+                {
+                    // ((*it)->attrptr[i])->print();
+                    // cout << "";
+                    // temp->attrptr[i]->print();
+                    // cout << "\t";
+                    // counting the number of common recored entries
+                    if (*((*it)->attrptr[i]) == *(temp->attrptr[i]))
+                    {
+                        count++;
+                    }
+                    // cout << count;
+                    if (count == R1->natttr)
+                    {
+                        flag = 0;
+                        break;
+                    }
                 }
+            }
+            if (flag == 1)
+            {
+                R->recs.push_back(*it);
+            }
         }
+        return R;
     }
     // 6. Rename: rename an attribute in schema
     Relation *_rename(Relation *R, const string &s1, const string &s2)
@@ -114,7 +170,7 @@ public:
                 break;
             }
         }
-        if(i == R->natttr)
+        if (i == R->natttr)
         {
             cout << "NO such Attribute with the name \" " << s1 << " \" found!" << endl;
         }
